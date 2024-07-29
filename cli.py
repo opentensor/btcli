d440fd71f19730e9cda75b9bb3bb9d3fac99b679
Benjamin Himes <benhimes@opentensor.dev>
2024-07-29 23:56:08 +0200

    TODO

--- cli.py
+++ cli.py
@@ -275,6 +275,7 @@ class CLIManager:
         comprehensive view of the user's network presence, making it ideal for monitoring account status, stake distribution,
         and overall contribution to the Bittensor network.
         """
+        # TODO verify this is actually doing all wallets
         if include_hotkeys and exclude_hotkeys:
             utils.err_console.print(
                 "[red]You have specified hotkeys for inclusion and exclusion. Pick only one or neither."
