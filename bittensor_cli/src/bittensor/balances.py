13fc5878107e795b0ed47e8f0bf258bcdf6ac9e2
Benjamin Himes <benhimes@opentensor.dev>
2024-09-27 12:46:14 +0200

    Balance update.

--- bittensor_cli/src/bittensor/balances.py
+++ bittensor_cli/src/bittensor/balances.py
@@ -18,6 +18,7 @@
 # DEALINGS IN THE SOFTWARE.
 
 from typing import Union
+from bittensor_cli.src import UNITS
 
 
 class Balance:
@@ -279,3 +280,21 @@ class Balance:
         :return: A Balance object representing the given amount.
         """
         return Balance(amount)
+
+    @staticmethod
+    def get_unit(netuid: int):
+        units = UNITS
+        base = len(units)
+        if netuid < base:
+            return units[netuid]
+        else:
+            result = ""
+            while netuid > 0:
+                result = units[netuid % base] + result
+                netuid //= base
+            return result
+
+    def set_unit(self, netuid: int):
+        self.unit = Balance.get_unit(netuid)
+        self.rao_unit = Balance.get_unit(netuid)
+        return self
