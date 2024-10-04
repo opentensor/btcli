e40f695a651be41aa2f3d9a5f8bcbd405e6243c2
Benjamin Himes <benhimes@opentensor.dev>
2024-10-04 13:48:51 +0200

    dumpster fire

--- bittensor_cli/src/bittensor/chain_data.py
+++ bittensor_cli/src/bittensor/chain_data.py
@@ -515,6 +515,76 @@ class DelegateInfo:
             results.append((delegate, Balance.from_rao(b)))
         return results
 
+@dataclass
+class DelegateInfoLite:
+    """
+    Dataclass for light delegate information.
+
+    Args:
+        hotkey_ss58 (str): Hotkey of the delegate for which the information is being fetched.
+        owner_ss58 (str): Coldkey of the owner.
+        total_stake (int): Total stake of the delegate.
+        owner_stake (int): Own stake of the delegate.
+        take (float): Take of the delegate as a percentage. None if custom
+    """
+
+    hotkey_ss58: str  # Hotkey of delegate
+    owner_ss58: str  # Coldkey of owner
+    take: Optional[float]
+    total_stake: Balance  # Total stake of the delegate
+    previous_total_stake: Optional[Balance]  # Total stake of the delegate
+    owner_stake: Balance  # Own stake of the delegate
+
+    @classmethod
+    def fix_decoded_values(cls, decoded: Any) -> "DelegateInfoLite":
+        """Fixes the decoded values."""
+        decoded_take = decoded["take"]
+
+        if decoded_take == 65535:
+            fixed_take = None
+        else:
+            fixed_take = u16_normalized_float(decoded_take)
+
+        return cls(
+            hotkey_ss58=ss58_encode(
+                decoded["delegate_ss58"], SS58_FORMAT
+            ),
+            owner_ss58=ss58_encode(decoded["owner_ss58"], SS58_FORMAT),
+            take=fixed_take,
+            total_stake=Balance.from_rao(decoded["total_stake"]),
+            owner_stake=Balance.from_rao(decoded["owner_stake"]),
+            previous_total_stake=None,
+        )
+
+    @classmethod
+    def from_vec_u8(cls, vec_u8: List[int]) -> Optional["DelegateInfoLite"]:
+        """Returns a DelegateInfoLight object from a ``vec_u8``."""
+        if len(vec_u8) == 0:
+            return None
+
+        decoded = from_scale_encoding(vec_u8, ChainDataType.DelegateInfoLight)
+
+        if decoded is None:
+            return None
+
+        decoded = DelegateInfoLite.fix_decoded_values(decoded)
+
+        return decoded
+
+    @classmethod
+    def list_from_vec_u8(cls, vec_u8: list[int]) -> list["DelegateInfoLite"]:
+        """Returns a list of DelegateInfoLight objects from a ``vec_u8``."""
+        decoded = from_scale_encoding(
+            vec_u8, ChainDataType.DelegateInfoLight, is_vec=True
+        )
+
+        if decoded is None:
+            return []
+
+        decoded = [DelegateInfoLite.fix_decoded_values(d) for d in decoded]
+
+        return decoded
+
 
 @dataclass
 class SubnetInfo:
