27444d90ea570782cb15ad545ee42849e60ca48d
Benjamin Himes <benhimes@opentensor.dev>
2024-10-10 20:47:32 +0200

    Fixes issue from #131 where "rpc_request" was not changed to `payload_id` in the error handler.

--- bittensor_cli/src/bittensor/async_substrate_interface.py
+++ bittensor_cli/src/bittensor/async_substrate_interface.py
@@ -1708,7 +1708,7 @@ class AsyncSubstrateInterface:
         result = await self._make_rpc_request(payloads, runtime=runtime)
         if "error" in result[payload_id][0]:
             raise SubstrateRequestException(
-                result["rpc_request"][0]["error"]["message"]
+                result[payload_id][0]["error"]["message"]
             )
         if "result" in result[payload_id][0]:
             return result[payload_id][0]
