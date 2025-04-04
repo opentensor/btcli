3b94054810bb5c00ae7b718f664fb62ab7605e7e
Benjamin Himes <benhimes@opentensor.dev>
2025-04-04 16:07:15 +0200

    New color palette

--- bittensor_cli/src/__init__.py
+++ bittensor_cli/src/__init__.py
@@ -746,23 +746,26 @@ class ColorPalette(Gettable):
         self.SU = self.SUDO
 
     class General(Gettable):
-        HEADER = "#4196D6"  # Light Blue
-        LINKS = "#8CB9E9"  # Sky Blue
-        HINT = "#A2E5B8"  # Mint Green
-        COLDKEY = "#9EF5E4"  # Aqua
-        HOTKEY = "#ECC39D"  # Light Orange/Peach
-        SUBHEADING_MAIN = "#7ECFEC"  # Light Cyan
-        SUBHEADING = "#AFEFFF"  # Pale Blue
-        SUBHEADING_EXTRA_1 = "#96A3C5"  # Grayish Blue
-        SUBHEADING_EXTRA_2 = "#6D7BAF"  # Slate Blue
-        CONFIRMATION_Y_N_Q = "#EE8DF8"  # Light Purple/Pink
-        SYMBOL = "#E7CC51"  # Gold
-        BALANCE = "#4F91C6"  # Medium Blue
-        COST = "#53B5A0"  # Teal
-        SUCCESS = "#53B5A0"  # Teal
-        NETUID = "#CBA880"  # Tan
-        NETUID_EXTRA = "#DDD5A9"  # Light Khaki
-        TEMPO = "#67A3A5"  # Grayish Teal
+        HEADER = "#57878B"  # mid blue green
+        LINKS = "#8CAAAE"  # Pale Blue
+        HINT = "#6C8871"  # Muted Green
+        COLDKEY = "#676B72"  # Dark grey blue
+        HOTKEY = "#747065"  # Dark orange grey
+        SUBHEADING_MAIN = "#8AA499"  # muted Blue green
+        SUBHEADING = "#9BAC9C"  # pale green
+        SUBHEADING_EXTRA_1 = "#C596A3"  # Dusty Rose
+        SUBHEADING_EXTRA_2 = "#9BAC9C"  # pale green
+        CONFIRMATION_Y_N_Q = "#EFB7AB"  # pale Pink
+        SYMBOL = "#FE917A"  # Salmon orange
+        SUBNET_NAME = "#C596A3"  # Dusty Rose
+        VALIDATOR_NAME = "#9BAC9C"  # Mid Lime Green
+        MINER_NAME = "#A17E7E"  # Dusty Red
+        BALANCE = "#757B7B"  # Muted teal Blue
+        COST = "#8EB27A"  # Green
+        SUCCESS = "#3D7F71"  # dark Teal
+        NETUID = "#BDC1C6"  # GREY_400
+        NETUID_EXTRA = "#D4D0C1"  # Light yellow grey
+        TEMPO = "#927A71"  # dark tan brown
         # aliases
         CK = COLDKEY
         HK = HOTKEY
@@ -777,25 +780,25 @@ class ColorPalette(Gettable):
         STAKE_AMOUNT = "#53B5A0"  # Teal
         STAKE_ALPHA = "#53B5A0"  # Teal
         STAKE_SWAP = "#67A3A5"  # Grayish Teal
-        TAO = "#4F91C6"  # Medium Blue
-        SLIPPAGE_TEXT = "#C25E7C"  # Rose
-        SLIPPAGE_PERCENT = "#E7B195"  # Light Coral
-        NOT_REGISTERED = "#EB6A6C"  # Salmon Red
-        EXTRA_1 = "#D781BB"  # Pink
+        TAO = "#8AA499"  # faded Blue green
+        SLIPPAGE_TEXT = "#BA938A"  # Brown Salmon
+        SLIPPAGE_PERCENT = "#CD8B7B"  # Muted Salmon
+        NOT_REGISTERED = "#A87D7D"  # Medium Red brown
+        EXTRA_1 = "#A45E44"  # Deep autumn orange
         # aliases
         AMOUNT = STAKE_AMOUNT
         ALPHA = STAKE_ALPHA
         SWAP = STAKE_SWAP
 
     class Pools(Gettable):
-        TAO = "#4F91C6"  # Medium Blue
-        ALPHA_IN = "#D09FE9"  # Light Purple
-        ALPHA_OUT = "#AB7CC8"  # Medium Purple
-        RATE = "#F8D384"  # Light Orange
-        TAO_EQUIV = "#8CB9E9"  # Sky Blue
-        EMISSION = "#F8D384"  # Light Orange
-        EXTRA_1 = "#CAA8FB"  # Lavender
-        EXTRA_2 = "#806DAF"  # Dark Purple
+        TAO = "#8AA499"  # faded Blue green
+        ALPHA_IN = "#C1913C"  # Mustard
+        ALPHA_OUT = "#B49766"  # Khaki Mustard
+        RATE = "#A46844"  # Deep orange
+        TAO_EQUIV = "#93BBAF"  # Teal Blue
+        EMISSION = "#B58065"  # Med Orange
+        EXTRA_1 = "#919170"  # Autumn green
+        EXTRA_2 = "#667862"  # Forest green
 
     class Grey(Gettable):
         GREY_100 = "#F8F9FA"  # Almost White
@@ -820,9 +823,9 @@ class ColorPalette(Gettable):
         G_900 = GREY_900
 
     class Sudo(Gettable):
-        HYPERPARAMETER = "#4F91C6"  # Medium Blue
-        VALUE = "#D09FE9"  # Light Purple
-        NORMALIZED = "#AB7CC8"  # Medium Purple
+        HYPERPARAMETER = "#93AFA3"  # Forest
+        VALUE = "#B58065"  # Burnt orange
+        NORMALIZED = "#A87575"  # Burnt Red
         # aliases
         HYPERPARAM = HYPERPARAMETER
         NORMAL = NORMALIZED
