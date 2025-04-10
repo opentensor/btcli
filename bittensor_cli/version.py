498742ac6a31188b7dafbdce3d3f90e38f7d7be9
Dmitry <98899785+mdqst@users.noreply.github.com>
2025-04-10 20:47:56 +0300

    chore: fixed version format error and improved readability

--- bittensor_cli/version.py
+++ bittensor_cli/version.py
@@ -3,18 +3,21 @@ import re
 
 
 def version_as_int(version):
-    _core_version = re.match(r"^\d+\.\d+\.\d+", version).group(0)
+    match = re.match(r"^\d+\.\d+\.\d+", version)
+    if not match:
+        raise ValueError(f"Invalid version format: {version}")
+    
+    _core_version = match.group(0)
     _version_split = _core_version.split(".")
-    __version_info__ = tuple(int(part) for part in _version_split)
-    _version_int_base = 1000
-    assert max(__version_info__) < _version_int_base
+    version_info = tuple(int(part) for part in _version_split)
+    version_int_base = 1000
+    assert max(version_info) < version_int_base
 
-    __version_as_int__: int = sum(
-        e * (_version_int_base**i) for i, e in enumerate(reversed(__version_info__))
+    version_as_int = sum(
+        e * (version_int_base**i) for i, e in enumerate(reversed(version_info))
     )
-    assert __version_as_int__ < 2**31  # fits in int32
-    __new_signature_version__ = 360
-    return __version_as_int__
+    assert version_as_int < 2**31  # fits in int32
+    return version_as_int
 
 
 __version__ = importlib.metadata.version("bittensor-cli")
