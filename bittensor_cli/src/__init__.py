3c075ebff4896d0d8cab587f5f229c38818ded0e
bdhimes <b@latent.to>
2025-09-19 15:54:40 +0200

    Revert "Revert "Merge pull request #628 from opentensor/feat/thewhaleking/trim-uids""
    This reverts commit b9d8cd325905f950903d1fbe7b831d5a04207ae2.

--- bittensor_cli/src/__init__.py
+++ bittensor_cli/src/__init__.py
@@ -666,6 +666,7 @@ HYPERPARAMS = {
     "user_liquidity_enabled": ("toggle_user_liquidity", True),
     "bonds_reset_enabled": ("sudo_set_bonds_reset_enabled", False),
     "transfers_enabled": ("sudo_set_toggle_transfer", False),
+    "min_allowed_uids": ("sudo_set_min_allowed_uids", True),
 }
 
 HYPERPARAMS_MODULE = {
