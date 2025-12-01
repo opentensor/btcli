9881a35a80192e8791d3753329bf7cfdb74cc260
bdhimes <b@latent.to>
2025-12-01 17:27:47 +0200

    Fixed tests

--- tests/e2e_tests/test_unstaking.py
+++ tests/e2e_tests/test_unstaking.py
@@ -387,9 +387,7 @@ def test_unstaking(local_chain, wallet_setup):
         ],
     )
 
-    assert (
-        "✅ Finalized: Successfully unstaked all Alpha stakes" in unstake_alpha.stdout
-    )
+    assert "✅ Included: Successfully unstaked all Alpha stakes" in unstake_alpha.stdout
     assert "Your extrinsic has been included" in unstake_alpha.stdout, (
         unstake_alpha.stdout
     )
@@ -442,6 +440,6 @@ def test_unstaking(local_chain, wallet_setup):
             "144",
         ],
     )
-    assert "✅ Finalized: Successfully unstaked all stakes from" in unstake_all.stdout
+    assert "✅ Included: Successfully unstaked all stakes from" in unstake_all.stdout
     assert "Your extrinsic has been included" in unstake_all.stdout, unstake_all.stdout
     print("Passed unstaking tests 🎉")
