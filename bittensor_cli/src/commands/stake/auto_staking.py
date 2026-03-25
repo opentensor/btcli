1e4be77e8e12981873a9f8ad1ad1f9d1dfa78642
ibraheem-latent <i@latent.to>
2026-03-25 16:34:28 -0700

    update set_auto_stake_destination

--- bittensor_cli/src/commands/stake/auto_staking.py
+++ bittensor_cli/src/commands/stake/auto_staking.py
@@ -45,7 +45,6 @@ async def show_auto_stake_destinations(
             subnet_info,
             auto_destinations,
             identities,
-            delegate_identities,
         ) = await asyncio.gather(
             subtensor.all_subnets(block_hash=chain_head),
             subtensor.get_auto_stake_destinations(
@@ -54,13 +53,11 @@ async def show_auto_stake_destinations(
                 reuse_block=True,
             ),
             subtensor.fetch_coldkey_hotkey_identities(block_hash=chain_head),
-            subtensor.get_delegate_identities(block_hash=chain_head),
         )
 
     subnet_map = {info.netuid: info for info in subnet_info}
     auto_destinations = auto_destinations or {}
     identities = identities or {}
-    delegate_identities = delegate_identities or {}
     hotkey_identities = identities.get("hotkeys", {})
 
     def resolve_identity(hotkey: str) -> Optional[str]:
@@ -72,10 +69,6 @@ async def show_auto_stake_destinations(
             display_name = identity_entry.get("name") or identity_entry.get("display")
             if display_name:
                 return display_name
-
-        delegate_info = delegate_identities.get(hotkey)
-        if delegate_info and getattr(delegate_info, "display", ""):
-            return delegate_info.display
 
         return None
 
@@ -183,10 +176,9 @@ async def set_auto_stake_destination(
 
     try:
         chain_head = await subtensor.substrate.get_chain_head()
-        subnet_info, identities, delegate_identities = await asyncio.gather(
+        subnet_info, identities = await asyncio.gather(
             subtensor.subnet(netuid, block_hash=chain_head),
             subtensor.fetch_coldkey_hotkey_identities(block_hash=chain_head),
-            subtensor.get_delegate_identities(block_hash=chain_head),
         )
     except ValueError:
         print_error(f"Subnet with netuid {netuid} does not exist")
@@ -194,17 +186,12 @@ async def set_auto_stake_destination(
 
     hotkey_identity = ""
     identities = identities or {}
-    delegate_identities = delegate_identities or {}
 
     hotkey_identity_entry = identities.get("hotkeys", {}).get(hotkey_ss58, {})
     if identity_data := hotkey_identity_entry.get("identity"):
         hotkey_identity = (
             identity_data.get("name") or identity_data.get("display") or ""
         )
-    if not hotkey_identity:
-        delegate_info = delegate_identities.get(hotkey_ss58)
-        if delegate_info and getattr(delegate_info, "display", ""):
-            hotkey_identity = delegate_info.display
 
     if prompt_user and not json_output:
         table = create_table(
